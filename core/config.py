6627566e89fe1f7f765f511e6364a5b1e62f31dc
blue-samarth <samarth38work@gmail.com>
2024-11-07 16:52:07 +0530

    Add CI/CD pipeline for Docker Compose

--- core/config.py
+++ core/config.py
@@ -4,7 +4,7 @@ import secrets
 def generate_secret_key():
     return secrets.token_urlsafe(32)
 
-if not os.path.exists('.env'):
-    with open('.env', 'w') as f:
+if not os.path.exists('core/.env'):
+    with open('core/.env', 'w') as f:
         f.write(f'SECRET_KEY={str(generate_secret_key())}\n')
 
