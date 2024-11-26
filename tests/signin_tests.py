be4c951580ef17a158c96bdddb0c619371303c86
blue-samarth <samarth38work@gmail.com>
2024-11-26 18:48:55 +0530

    Tests

--- tests/signin_tests.py
+++ tests/signin_tests.py
@@ -23,4 +23,31 @@ async def test_signin(async_client):
     assert response.status_code == 200
     assert response.json()["status"] == "success"
     assert response.json()["message"] == "Student signed in successfully"
+    # db.students.delete_many({"email": "test@example.com"})
+
+
+async def test_incorrectpass_signup(async_client):
+    response = await async_client.post("/student/signin",
+            json={
+                'email': 'test@example.com',
+                'password': 'testpassword12'
+            }
+        )
+    print(response.json())
+    assert response.status_code == 401
+    assert response.json()["status"] == "error"
+    assert response.json()["message"] == "Invalid password"
+
+
+async def test_notfound_signup(async_client):
+    response = await async_client.post("/student/signin",
+            json={
+                'email': 'test@exampl.com',
+                'password': 'testpassword123'
+            }
+        )
+    print(response.json())
+    assert response.status_code == 404
+    assert response.json()["status"] == "error"
+    assert response.json()["message"] == "Student not found"
     db.students.delete_many({"email": "test@example.com"})
